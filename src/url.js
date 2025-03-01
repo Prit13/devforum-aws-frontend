0db8cc88be911775968c37098a4355d9d6507d40
Prit13 <beingstarx77@gmail.com>
2025-03-01 22:27:45 +0530

    Got ssl certificate for backend server

--- src/url.js
+++ src/url.js
@@ -1,6 +1,6 @@
 // export const URL="http://localhost:5000"         // use in local port 
-export const URL="http://prit33.duckdns.org:8080"
+export const URL="https://prit33.duckdns.org"
 
 // export const IF="http://localhost:5000/file/"
-export const IF="http://prit33.duckdns.org:8080/file/"
+export const IF="https://prit33.duckdns.org/file/"
 
